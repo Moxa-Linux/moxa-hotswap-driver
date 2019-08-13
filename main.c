27878565e1126d821d542da21aa493fadf55a0ce
ElvisCW_Yao <ElvisCW.Yao@moxa.com>
2019-08-13 16:34:17 +0800

    style: fix some coding style in main.c

--- main.c
+++ main.c
@@ -23,8 +23,8 @@
 #include <asm/io.h>
 #include <linux/spinlock.h>
 #include "mxhtsp_ioctl.h"
-#include "moxa_hotswap.h" 
-#include "moxa_superio.h" 
+#include "moxa_hotswap.h"
+#include "moxa_superio.h"
 
 MODULE_AUTHOR("ElvisCW.Yao@moxa.com");
 MODULE_LICENSE("GPL");
@@ -35,11 +35,12 @@ static void *vaddr; /* virtual memory address */
 static struct timer_list timer;
 static disk_info d1, d2;
 
-static int hotswap_led_control(int led_num, int on) {
+static int hotswap_led_control(int led_num, int on)
+{
 	int led_bit;
 	unsigned char pled_status;
 
-	if (((led_num != 1) && (led_num !=2)) || ((on != 1) && (on != 0)))
+	if (((led_num != 1) && (led_num != 2)) || ((on != 1) && (on != 0)))
 		return -1;
 
 	if (led_num == 1) {
@@ -60,19 +61,19 @@ static int hotswap_led_control(int led_num, int on) {
 	return 0;
 }
 
-static int hotswap_open(struct inode *inode, struct file *file) 
+static int hotswap_open(struct inode *inode, struct file *file)
 {
 	p("\n");
 	return 0;
 }
 
-static int hotswap_release(struct inode *inode, struct file *file) 
+static int hotswap_release(struct inode *inode, struct file *file)
 {
 	p("\n");
 	return 0;
 }
 
-static long hotswap_ioctl(struct file *file, unsigned int cmd, unsigned long arg) 
+static long hotswap_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	void __user *addr = (void __user *)arg;
 	int err = 0;
@@ -83,79 +84,72 @@ static long hotswap_ioctl(struct file *file, unsigned int cmd, unsigned long arg
 		return -ENOTTY;
 
 	if (_IOC_DIR(cmd) & _IOC_READ) {
-		err = !access_ok(VERIFY_WRITE, (void __user*)arg, _IOC_SIZE(cmd));
+		err = !access_ok(VERIFY_WRITE, (void __user *)arg, _IOC_SIZE(cmd));
 	} else if (_IOC_DIR(cmd) & _IOC_WRITE) {
 		err = !access_ok(VERIFY_READ, (void __user *)arg, _IOC_SIZE(cmd));
 	}
 	if (err)
-	        return -EFAULT;
+		return -EFAULT;
 
 	switch (cmd) {
 		case IOCTL_GET_DISK_STATUS:
 			if (copy_from_user(&p, addr, sizeof(mxhtsp_param)))
 				goto err;
-			if (p.disk_num == 1) 
+			if (p.disk_num == 1)
 				p.val = d1.busy;
-			else if (p.disk_num == 2) 
+			else if (p.disk_num == 2)
 				p.val = d2.busy;
-			else 
+			else
 				return -EINVAL;
-			if (copy_to_user(addr, &p, sizeof(mxhtsp_param)))  
+			if (copy_to_user(addr, &p, sizeof(mxhtsp_param)))
 				goto err;
 			break;
 
-		case IOCTL_SET_DISK_LED : 
-			if (copy_from_user(&p, addr, sizeof(mxhtsp_param))) 
+		case IOCTL_SET_DISK_LED:
+			if (copy_from_user(&p, addr, sizeof(mxhtsp_param)))
 				goto err;
 			return hotswap_led_control(p.disk_num, p.val);
 			break;
 
-		case IOCTL_GET_DISK_BTN :
-			if (copy_from_user(&p, addr, sizeof(mxhtsp_param))) 
+		case IOCTL_GET_DISK_BTN:
+			if (copy_from_user(&p, addr, sizeof(mxhtsp_param)))
 				goto err;
 			if (p.disk_num == 1) {
 				p.val = moxainb(LED_BTN_ADDR);
 				/* if BIT_DISK1_BTN is 0 (button pressed), set p.val to 1 to indicated the button is pressed */
-				p.val = ( (~p.val) & BIT_DISK1_BTN) ? 1 : 0 ;
-			}
-			else if (p.disk_num == 2) {
+				p.val = ((~p.val) & BIT_DISK1_BTN) ? 1 : 0 ;
+			} else if (p.disk_num == 2) {
 				p.val = moxainb(LED_BTN_ADDR);
-				p.val = ( (~p.val) & BIT_DISK2_BTN) ? 1 : 0 ;
-			}
-			else 
+				p.val = ((~p.val) & BIT_DISK2_BTN) ? 1 : 0 ;
+			} else
 				return -EINVAL;
-			if (copy_to_user(addr, &p, sizeof(mxhtsp_param))) 
+			if (copy_to_user(addr, &p, sizeof(mxhtsp_param)))
 				goto err;
 			break;
-		
-		case IOCTL_CHECK_DISK_PLUGGED :
+
+		case IOCTL_CHECK_DISK_PLUGGED:
 			if (copy_from_user(&p, addr, sizeof(mxhtsp_param))) {
 				goto err;
 			}
 			if (p.disk_num == 1) {
-				p.val = (ioread32(vaddr + SATA_P1SSTS) == 0x4) ? 0: 1;/* 0x4 for un-plug */
-				if (p.val == 1) {
-					p.val = (ioread32(vaddr + SATA_P1SSTS) == 0x00) ? 0: 1;/* TODO: BIOS disable hotswap */
-				}
-
-			}
-			else if (p.disk_num == 2) {
-				p.val = (ioread32(vaddr + SATA_P2SSTS) == 0x4) ? 0: 1;/* 0x4 for un-plug */
-				if (p.val == 1) {
-                                        p.val = (ioread32(vaddr + SATA_P2SSTS) == 0x00) ? 0: 1;/* TODO: BIOS disable hotswap */
-                                }
-			}
-			else 
+				p.val = (ioread32(vaddr + SATA_P1SSTS) == 0x4) ? 0 : 1;/* 0x4 for un-plug */
+				if (p.val == 1)
+					p.val = (ioread32(vaddr + SATA_P1SSTS) == 0x00) ? 0 : 1;/* TODO: BIOS disable hotswap */
+			} else if (p.disk_num == 2) {
+				p.val = (ioread32(vaddr + SATA_P2SSTS) == 0x4) ? 0 : 1;/* 0x4 for un-plug */
+				if (p.val == 1)
+					p.val = (ioread32(vaddr + SATA_P2SSTS) == 0x00) ? 0 : 1;/* TODO: BIOS disable hotswap */
+			} else
 				return -EINVAL;
-			if (copy_to_user(addr, &p, sizeof(mxhtsp_param))) 
+			if (copy_to_user(addr, &p, sizeof(mxhtsp_param)))
 				goto err;
 			break;
 
-		default :
+		default:
 			mprintk("ioctl error\n");
 			return -EINVAL;
 	}
-	return 0; 
+	return 0;
 
 err:
 	mprintk("copy data error\n");
@@ -169,28 +163,28 @@ ssize_t hotswap_write(struct file *file, const char __user *buf, size_t count,
 	int led_num;
 	int on;
 
-	if (count != 4) 
+	if (count != 4)
 		return -1;
 
-	if(copy_from_user(param, buf, count)) 
+	if (copy_from_user(param, buf, count))
 		return -1;
 
 	p("count=%d param[3]=%x\n", count, param[3]);
 	param[3] = '\0'; /* change \n to \0 */
 	sscanf(param, "%d %d", &led_num, &on);
 
-	if (hotswap_led_control(led_num, on)) 
+	if (hotswap_led_control(led_num, on))
 		return -1;
 
 	return count;
 }
 
 static struct file_operations hotswap_fops = {
-	.owner	=	THIS_MODULE,
-	.unlocked_ioctl  = hotswap_ioctl,
-	.open	=	hotswap_open,
-	.write	=	hotswap_write,
-	.release=	hotswap_release,
+	.owner		=	THIS_MODULE,
+	.unlocked_ioctl =	hotswap_ioctl,
+	.open		=	hotswap_open,
+	.write		=	hotswap_write,
+	.release	=	hotswap_release,
 };
 
 static struct miscdevice hotswap_dev = {
@@ -199,8 +193,8 @@ static struct miscdevice hotswap_dev = {
 	.fops = &hotswap_fops,
 };
 
-
-static void hotswap_check_disk(unsigned long arg) {
+static void hotswap_check_disk(unsigned long arg)
+{
 	unsigned long now;
 
 	now = ioread32(vaddr+SATA_P1SSTS);
@@ -227,11 +221,11 @@ static void hotswap_check_disk(unsigned long arg) {
 
 	mod_timer(&timer, jiffies+100*HZ/1000);
 
- 	p("disk 0x128=%x, 0x1A8=%x, 0x228=%x, 0x2A8=%x\n",
-		ioread32(vaddr+0x128), ioread32(vaddr+0x1A8), ioread32(vaddr+0x228),ioread32(vaddr+0x2A8));
+	p("disk 0x128=%x, 0x1A8=%x, 0x228=%x, 0x2A8=%x\n",
+		ioread32(vaddr+0x128), ioread32(vaddr+0x1A8), ioread32(vaddr+0x228), ioread32(vaddr+0x2A8));
 }
 
-static int __init hotswap_init_module (void) 
+static int __init hotswap_init_module (void)
 {
 	struct pci_dev *dev;
 
@@ -248,12 +242,12 @@ static int __init hotswap_init_module (void)
 
 #ifdef DEBUG
 	unsigned long flag = pci_resource_flags(dev, 5);
-	if (flag & IORESOURCE_IO) 
+	if (flag & IORESOURCE_IO)
 		printk("ioresource io\n");
-	else if (flag & IORESOURCE_MEM) 
+	else if (flag & IORESOURCE_MEM)
 		printk("ioresource mem\n");
 #endif
-	/* register in /proc/iomem, 1024 can count in the same file 
+	/* register in /proc/iomem, 1024 can count in the same file
 	if (!request_mem_region(paddr, 1024, "hotswap")) {
 		mprintk("can't reguest mem region");
 	}
@@ -275,7 +269,7 @@ static int __init hotswap_init_module (void)
 
 	init_timer(&timer);
 	timer.data = 0;
-	timer.function = hotswap_check_disk; 
+	timer.function = hotswap_check_disk;
 	timer.expires = jiffies + 100*HZ/1000;
 	add_timer(&timer);
 
@@ -285,12 +279,12 @@ static int __init hotswap_init_module (void)
 /*
  * close and cleanup module
  */
-static void __exit hotswap_cleanup_module (void) 
+static void __exit hotswap_cleanup_module (void)
 {
 	p("\n");
 	misc_deregister(&hotswap_dev);
 	iounmap(vaddr);
-	//release_mem_region(paddr, 1024); 
+	/* release_mem_region(paddr, 1024); */
 	del_timer(&timer);
 }
 
